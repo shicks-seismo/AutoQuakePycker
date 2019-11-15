d87e8fe7c59b7df47255c094f0eba1677c22be2c
Stephen Hicks <s.hicks@soton.ac.uk>
2019-11-15 15:30:38 +0000

    docstrings

--- docs/build/html/_sources/index.rst.txt
+++ docs/build/html/_sources/index.rst.txt
@@ -8,7 +8,7 @@ Welcome to AutoQuakePycker's documentation!
 
 .. toctree::
    :maxdepth: 2
-   :caption: Contents:
+
    all-about-me
 
 
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,15 +10,14 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
 import sphinx_rtd_theme
 
 extensions = [
-    "sphinx_rtd_theme"
-]
+    "sphinx_rtd_theme", 'sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon']
 
 html_theme = "sphinx_rtd_theme"
 
