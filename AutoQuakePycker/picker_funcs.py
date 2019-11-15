c8dac10d35878eaea35e2a49682815b28bca0ba4
Stephen Hicks <s.hicks@soton.ac.uk>
2019-11-15 17:07:43 +0000

    tmp

--- AutoQuakePycker/picker_funcs.py
+++ AutoQuakePycker/picker_funcs.py
@@ -2,7 +2,15 @@ def CF_kurtosis(kurt_win_size, tr):
     """
     This is the Kurtosis function of Baillard et al. (2014, SRL)
     (Eq. 10). Sets values outside of window to NaN.
-    """
+
+    Parameters
+    ----------
+    kurt_win_size : kurtosis window length in seconds
+    tr : ObsPy trace object
+ 
+    Returns
+    ----------
+    cf : kurtosis characteristic function time series
     nwind = int(np.ceil(kurt_win_size * tr.stats.sampling_rate))
     cf = np.zeros(tr.stats.npts)
     for i in range(nwind, tr.stats.npts-1):
